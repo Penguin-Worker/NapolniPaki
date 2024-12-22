a7cb7926c9b0277da7fd7842903b6c3c72a0b060
Valdemar <nikiplaycar@gmail.com>
2024-12-22 05:20:08 +0300

    feat: add fileupload

--- server/controllers/goodsController.js
+++ server/controllers/goodsController.js
@@ -1,6 +1,23 @@
-class GoodsController {
-    async create(req,res){
+const uuid = require('uuid')
+const path = require('path')
+const { Goods } = require('../models/models')
+const ApiError = require('../error/ApiError')
 
+class GoodsController {
+    async create(req,res,next){
+        try{
+             const {name, price, brandId, typeId, info} = req.body
+        const {img} = req.files
+        let fileName = uuid.v4()+".jpg"
+        img.mv(path.resolve(__dirname,'..','static',fileName))
+
+        const goods = await Goods.create({name, price, brandId, typeId, img: fileName})
+
+        return res.json(goods)
+        }catch(e){
+            next(ApiError.badRequest(e.message))
+        }
+       
     }
     async getAll(req,res){
         
