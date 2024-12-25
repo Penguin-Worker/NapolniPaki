4111b241c78b099f4ee806e2ad18f428026dacd6
Valdemar <nikiplaycar@gmail.com>
2024-12-25 20:43:50 +0300

    feat: add authorization page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,1 +1,33 @@
-//BAZA
+import React, { useContext } from 'react';
+import { Context } from '..';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
+import { SHOP_ROUTE } from './utils/consts';
+import Button from 'react-bootstrap/Button';
+import { observer } from 'mobx-react-lite';
+import Container from 'react-bootstrap/Container';
+const NavBar=observer (() => {
+    const {user} = useContext(Context)
+  return (
+    <Navbar bg="dark" data-bs-theme="dark" >
+    
+      <NavLink className="ms-auto" style={{color:'white'}} to= {SHOP_ROUTE}> RedPlitka</NavLink>
+      <Container>
+      {user.isAuth ?
+      <Nav className="ms-auto" style={{color:'white'}}>
+        <Button variant='outline-light'>AdminPanel</Button>
+        <Button variant='outline-light' className="ms-2" onClick={()=> user.setIsAuth(false)}>Exit</Button>
+      </Nav>
+      :
+      <Nav className="ms-auto" style={{color:'white'}}>
+        
+        <Button variant={'outline-light'} onClick={()=> user.setIsAuth(true)} >Authorization</Button>
+      </Nav>
+      }   
+    </Container>
+  </Navbar>
+  );
+})
+
+export default NavBar;
--- client/src/components/pages/Auth.js
+++ client/src/components/pages/Auth.js
@@ -1,11 +1,47 @@
 import React from 'react';
-
-
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import { NavLink , useLocation} from 'react-router-dom';
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts';
 const Auth= () => {
+const location=useLocation()
+console.log(location)
+const isLogin=location.pathname===LOGIN_ROUTE
   return (
-    <div>
-      Auth
-    </div>
+    <Container className='d-flex justify-content-center align-items-center' style={{height:window.innerHeight-54}}>
+      <Card style={{width:600}} className="p-5">
+        <h2 className="m-auto">{isLogin ? 'Authorization' : 'Registration'}</h2>
+      <Form className='d-flex flex-column'>
+        <Form.Control
+        className='mt-3'
+        placeholder='email'        
+        />
+        <Form.Control
+        className='mt-3'
+        placeholder='password'        
+        />
+        <Row>
+          {isLogin ? 
+          <div>
+            Not registered yet <NavLink to={REGISTRATION_ROUTE}>Registration</NavLink>
+          </div>
+          :
+          <div>
+            Have account <NavLink to={LOGIN_ROUTE}>Login</NavLink>
+          </div>
+          }
+           <Button variant={'outline-success'} className='mt-2 align-self-end'>
+          {isLogin ? 'Login':'Registration'}
+        </Button>
+        </Row>
+       
+      </Form>
+      </Card>
+
+    </Container>
   );
 }
 
