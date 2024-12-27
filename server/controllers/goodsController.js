1e9d7788632e348a8cb695ffd5ee8aed3a1888fa
Valdemar <nikiplaycar@gmail.com>
2024-12-27 07:19:12 +0300

    fix: property bd read

--- server/controllers/goodsController.js
+++ server/controllers/goodsController.js
@@ -10,65 +10,55 @@ class GoodsController {
         try{
              let {name, price, brandId, typeId, info} = req.body
         const {img} = req.files
-        let fileName = uuid.v4()+".jpg"
+        let fileName = uuid.v4() + ".jpg"
         img.mv(path.resolve(__dirname,'..','static',fileName))
 
         const goods = await Goods.create({name, price, brandId, typeId, img: fileName})
 
-            if(info){
-                info = JSON.parse(info)
-                info.forEach(i=>
-                    GoodsInfo.create({
-                        title: i.title,
-                        description: i.description,
-                        goodsId: goods.id
-                    })
-                )
-            }
-
-
-
-
-        
-
+        if (info) {
+            info = JSON.parse(info);
+            
+            await Promise.all(info.map(i =>
+                GoodsInfo.create({
+                    title: i.title,
+                    description: i.description,
+                    goodId: goods.id
+                })
+            ));
+        }
         return res.json(goods)
         }catch(e){
             next(ApiError.badRequest(e.message))
         }
        
     }
-    async getAll(req,res){
-        const {brandId, typeId} = req.body
+    async getAll(req, res) {
+        const { brandId, typeId, page = 1, limit = 9 } = req.query;
         let goods;
-        page = page || 1
-        limit= limit || 9
-        let offset = page * limit - limit
-        if (!brandId && !typeId){
-            goods = await Goods.findAndCountAll({limit, offset})
-
+        let offset = (page - 1) * limit;
+    
+        if (!brandId && !typeId) {
+            goods = await Goods.findAndCountAll({ limit, offset });
         }
-        if (brandId && !typeId){
-            goods = await Goods.findAndCountAll({where:{brandId}, limit, offset})
-
+        if (brandId && !typeId) {
+            goods = await Goods.findAndCountAll({ where: { brandId }, limit, offset });
         }
-        if (!brandId && typeId){
-            goods = await Goods.findAndCountAll({where:{typeId}, limit, offset})
+        if (!brandId && typeId) {
+            goods = await Goods.findAndCountAll({ where: { typeId }, limit, offset });
         }
-        if (brandId && typeId){
-            goods = await Goods.findAndCountAll({where:{brandId, brandId}, limit, offset})
+        if (brandId && typeId) {
+            goods = await Goods.findAndCountAll({ where: { brandId, typeId }, limit, offset });
         }
-
-        return res.json(goods)
-
-
+    
+        return res.json(goods);
     }
     async getOne(req,res){
         const {id} = req.params
         const goods = await Goods.findOne(
             {
                 where: {id},
-                include: [{model:GoodsInfo, as:'info'}]
-            },
+                include: [{model:GoodsInfo, as: 'info' }]
+            }
         )
         return res.json(goods)
     }
